7aa0bd00f41b99978135a2422489dc976afaabea
JaVan33 <168095720+JaVan33@users.noreply.github.com>
2025-05-04 16:03:15 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import time
 
+print("Hello, this is a testing python file")
+time.sleep(2)
+print("What do you want to do?")
+time.sleep(1)
+input(">")
+time.sleep(2)
+print("That is great!")
+time.sleep(2)
+print("Sorry, i need to end the chat, sorry...")
+time.sleep(5)
